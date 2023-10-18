4add31e066855ebe5eb51e85b011986d4a960afd
Chongzhi Yin <yinchongzhi@gmail.com>
2023-10-18 21:18:31 +0800

    [add] setups for the rerun

--- scale-rm/test/case/shallowcloud/SDM64_50x50x5_no_sed_2.5e-2/SDM_job.sh
+++ scale-rm/test/case/shallowcloud/SDM64_50x50x5_no_sed_2.5e-2/SDM_job.sh
@@ -8,7 +8,7 @@
 #PJM -j
 #------- Program execution -------#
 module load intel impi hdf5 netcdf netcdf-fortran
-
+source ~/.bashrc
 # run
 mpiexec.hydra -n ${PJM_MPI_PROC} ./scale-rm_init init.conf || exit
 mpiexec.hydra -n ${PJM_MPI_PROC} ./scale-rm run.conf || exit
